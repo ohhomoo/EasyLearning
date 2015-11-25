30964e190dbe6d017f5b0aa4fb34b161e680aa53
ohhomoo <ohho7519@gmail.com>
2015-11-25 14:59:28 +0700

    Check Zero Login Success

--- app/src/main/java/com/ezgenius/easylearning/MainActivity.java
+++ app/src/main/java/com/ezgenius/easylearning/MainActivity.java
@@ -2,20 +2,56 @@ package com.ezgenius.easylearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private ManageTABLE objManageTABLE;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Bind Widget
+        bindWidget();
+
+
         // Connected Database
         objManageTABLE = new ManageTABLE(this);
 
     }   // onCreate
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+
+    }
+
+    public void clickLogin(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("")||passwordString.equals("")) {
+
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.myDialog(MainActivity.this, "มีช่องว่างนะครับ", "กรุณากรอกให้ครบทุกช่อง");
+
+        } else {
+
+            //No Space
+
+        }
+
+    }   // clickLogin
+
 
 }   // Main Class
